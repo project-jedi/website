2866a8273b28a5abd26f53076d7a5dda7644f288
obones <obones@5ab3729c-d286-496f-9ec8-17cde76a9afb>
2009-04-03 12:32:09 +0000

    Test the mail() function method

--- delphi-jedi.org/issuetracker/core/phpmailer/testphpmail.php
+++ delphi-jedi.org/issuetracker/core/phpmailer/testphpmail.php
@@ -4,36 +4,38 @@ require("class.phpmailer.php");
 
 $mail = new phpmailer();
 
-$mail->IsSMTP();     
+$mail->IsMail();
+
+/*$mail->IsSMTP();     
 $mail->Mailer   = "smtp";  
 $mail->Host     = "82.67.45.121"; // SMTP-Server
-$mail->Port     = 25;
-$mail->SMTPAuth = true;     // SMTP mit Authentifizierung benutzen
+$mail->Port     = 25;*/
+//$mail->SMTPAuth = true;     // SMTP mit Authentifizierung benutzen
 // $mail->SMTPDebug = true;
-$mail->SMTPAuth = true;
-$mail->Username = "jedi";  // SMTP-Benutzername
-$mail->Password = "jedimail"; // SMTP-Passwort
+//$mail->SMTPAuth = true;
+//$mail->Username = "jedi";  // SMTP-Benutzername
+//$mail->Password = "jedimail"; // SMTP-Passwort
+$mail->LE = "\n";
 $mail->From     = "webmaster@delphi-jedi.org";
 $mail->FromName = "Max Mustermann";
-$mail->AddAddress("ma.thoma@gmx.de","Herr Beispiel");
+$mail->AddAddress("obones@free.fr","fdsfds");
 
 $mail->WordWrap = 50;                              // Zeilenumbruch einstellen
 // $mail->AddAttachment("/var/tmp/file.tar.gz");      // Attachment
 // $mail->AddAttachment("/tmp/image.jpg", "new.jpg");
 $mail->IsHTML(true);                               // als HTML-E-Mail senden
 
-$mail->Subject  =  "Test mit PHPMailer";
-$mail->Body     =  "Test mit <b>PHPMailer</b>";
-$mail->AltBody  =  "Hallo Empfaenger, dies ist ein Test mit dem PHPMailer unter
-Linux und mit PHP ";
+$mail->Subject  =  "Test of PHPMailer";
+$mail->Body     =  "Test with <b>PHPMailer</b>";
+$mail->AltBody  =  "Hello, this is a test";
 
 if(!$mail->Send())
 {
-    echo "Die Nachricht konnte nicht versandt werden <p>";
+    echo "Unable to send email <p>";
     echo "Mailer Error: " . $mail->ErrorInfo;
     exit;
 }
 
-echo "Die Nachricht wurde erfolgreich versandt";
+echo "Email sent";
 
 ?>
